f21a1a5579c28835f114fb9d5c2d7bd66acd1cca
Jmins <jayminslove@gmail.com>
2022-09-18 13:32:41 +0900

    test

---
+++ python/sorce.007.py
@@ -0,0 +1,12 @@
+#자료형 출력 개념 배기
+a = 200
+msg = 'Now Im Doing Python Coding Study'
+list_data = ['a','b','c']
+dict_data = {'a':97,'b':98}
+print(a)
+print(msg)
+print(list_data)
+print(list_data[0])
+print(list_data[1])
+print(dict_data)
+print(dict_data['a'])
